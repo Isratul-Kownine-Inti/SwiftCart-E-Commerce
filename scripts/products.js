77126153945b4fb72fe04fe307fd87ad7aa4a858
Isratul-Kownine-Inti <isratulinti24@gmail.com>
2026-02-17 12:16:19 +0600

    active button portion completed

--- scripts/products.js
+++ scripts/products.js
@@ -1,55 +1,87 @@
 
-// load the categories 
-const loadCategories =() =>{
-    
-    const url = "https://fakestoreapi.com/products/categories";
-    fetch(url)
-    .then(res=>res.json())
-    .then(data =>displayCategories(data))
+const removeActive = () =>{
+    const categoryButtons = document.querySelectorAll(".category-buttons");
+    // console.log(categoryButtons);
+    categoryButtons.forEach(button => button.classList.remove("active"));
 }
 
-const displayCategories = (categories) =>{
-    
-    const categoryContainer = document.getElementById("categories");
-    // categoryContainer.innerHTML = "";
 
-    categories.forEach(category =>{
+// load the categories 
+const loadCategoriesButton = () => {
+
+    const url = "https://fakestoreapi.com/products/categories";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategoriesButton(data))
+}
+
+const displayCategoriesButton = (categories) => {
+
+    const categoryContainer = document.getElementById("categories");
+    categoryContainer.innerHTML = "";
+
+    const allButton = document.createElement("button");
+    allButton.className = "btn btn-primary rounded-full";
+    allButton.innerText = "All";
+    categoryContainer.appendChild(allButton);
+
+    allButton.addEventListener('click', ()=>{
+        loadAllProducts();
+    })
+
+    categories.forEach(category => {
         const cateGoryDiv = document.createElement("div");
         cateGoryDiv.innerHTML = `
 
         
-        <button onclick="loadCategoryProducts('${category.replace(/'/g, "\\'")}')" class="btn btn-outline">${category}</button>
+        <button id="category-btn-${category}" onclick="loadCategoryProducts('${category.replace(/'/g, "\\'")}')" class="btn btn-outline category-buttons">${category}</button>
 
         `
         categoryContainer.append(cateGoryDiv)
     })
 
 
-    
+
 }
+
+
 
 // load all products 
 
-const loadAllProducts = () =>{
+const loadAllProducts = () => {
     const url = "https://fakestoreapi.com/products";
 
     fetch(url)
-    .then(res=>res.json())
-    .then(data =>displayAllProducts(data))
+        .then(res => res.json())
+        .then(data => displayAllProducts(data))
 }
 
-// display all products 
-const displayAllProducts = (products) =>{
-    console.log(products)
-    
-    const allProductsContainer = document.getElementById("all-products-container");
-    allProductsContainer.innerHTML = "";
+//load category products
+const loadCategoryProducts = (category) => {
+    const url = `https://fakestoreapi.com/products/category/${category}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            removeActive();
+            const clickCategoryButton = document.getElementById(`category-btn-${category}`);
+            clickCategoryButton.classList.add("active");
+            displayAllProducts(data);
+        })
+}
 
-    products.forEach(product =>{
+
+// display all products and category products
+const displayAllProducts = (products) => {
+    console.log(products)
+
+    const productsContainer = document.getElementById("products-container");
+    productsContainer.innerHTML = "";
+
+    products.forEach(product => {
         console.log(product)
         const allProductsCard = document.createElement("div");
         allProductsCard.innerHTML = `
-              <div class="card bg-base-100 w-80 shadow-sm product-card">
+              <div class="card bg-base-100 w-full md:w-80 shadow-sm product-card">
                     <figure>
                         <img class="px-8 py-5 h-82 bg-gray-200 " src=${product.image} alt="backpack" />
                     </figure>
@@ -59,7 +91,7 @@ const displayAllProducts = (products) =>{
                             <span class="text-gray-500"><i class="fa-solid fa-star text-amber-500 mr-1"></i>${product.rating.rate}
                                 (${product.rating.count})</span>
                         </div>
-                        <h2 class="card-title">
+                        <h2 class="card-title truncate">
                             ${product.title}
 
                         </h2>
@@ -77,76 +109,27 @@ const displayAllProducts = (products) =>{
                 </div>
         
         `
-        allProductsContainer.append(allProductsCard)
+        productsContainer.append(allProductsCard)
     })
 }
 
 
-//load category products
-const loadCategoryProducts =(category) =>{
-    const url =`https://fakestoreapi.com/products/category/${category}`
+
+
+
+
+const loadProductDetails = (id) => {
+    const url = `https://fakestoreapi.com/products/${id}`
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayCategoryProducts(data))
+        .then(res => res.json())
+        .then(data => displayProductDetails(data))
 }
 
-// display category products 
-
-const displayCategoryProducts = (categoryProducts)=>{
-    console.log(categoryProducts);
-
-    const categoryProductsContainer = document.getElementById("category-products");
-    categoryProductsContainer.innerHTML = "";
-
-    categoryProducts.forEach(categoryProduct =>{
-         console.log(categoryProduct);
-         const categoryProductsCard = document.createElement("div");
-         categoryProductsCard.innerHTML = `
-              <div class="card bg-base-100 w-80 shadow-sm product-card">
-                    <figure>
-                        <img class="px-8 py-5 h-82 bg-gray-200 " src=${categoryProduct.image} alt="backpack" />
-                    </figure>
-                    <div class="card-body">
-                        <div class="flex justify-between mt-2">
-                            <div class="badge badge-soft badge-primary font-semibold">${categoryProduct.category}</div>
-                            <span class="text-gray-500"><i class="fa-solid fa-star text-amber-500 mr-1"></i>${categoryProduct.rating.rate}
-                                (${categoryProduct.rating.count})</span>
-                        </div>
-                        <h2 class="card-title">
-                            ${categoryProduct.title}
-
-                        </h2>
-                        <p class="font-bold text-xl">$${categoryProduct.price}</p>
-                        <div class="card-actions justify-between">
-
-                            <button onclick="loadProductDetails(${categoryProduct.id})" class="btn btn-outline px-5"><i class="fa-regular fa-eye"></i>Details</button>
-                            <button class="btn btn-primary px-10"><i class="fa-solid fa-cart-shopping"></i>Add</button>
-
-
-
-
-                        </div>
-                    </div>
-                </div>
-         `
-
-         categoryProductsContainer.append(categoryProductsCard);
-    })
-}
-
-
-const loadProductDetails = (id) =>{
-    const url =`https://fakestoreapi.com/products/${id}`
-    fetch(url)
-    .then(res=>res.json())
-    .then(data =>displayProductDetails(data))
-}
-
-const displayProductDetails = (product)=>{
+const displayProductDetails = (product) => {
     console.log(product)
 
     const detailContainer = document.getElementById("detail-container");
-    detailContainer.innerHTML =` <div class="space-y-3">
+    detailContainer.innerHTML = ` <div class="space-y-3">
         <h2 class="font-bold text-xl">${product.title}</h2>
         <p class="font-medium text-gray-600">${product.description}</p>
         <span class="font-bold">$${product.price}</span>
@@ -158,5 +141,5 @@ const displayProductDetails = (product)=>{
 
 }
 
-loadCategories()
+loadCategoriesButton()
 // loadAllProducts()
